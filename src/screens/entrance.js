a906069758b309522ac593047ea4c10d60cf31e9
asaba <swift0910@cpoint-lab.co.jp>
2020-10-15 18:28:14 +0900

    ボタンを押したら選択肢のあるモーダルを追加

--- src/screens/entrance.js
+++ src/screens/entrance.js
@@ -3,26 +3,35 @@ import { View,ImageBackground } from 'react-native';
 import CircleButton from '../../src/elements/atom/CircleButton';
 import {Actions} from 'react-native-router-flux';
 import entranceStyle from '../../src/style/entranceStyle';
+import SelectModal from '../elements/molecules/SelectModal';
 
 class entrance extends Component {
   constructor(props) {
     super(props);
     this.state = {
+      isModalVisible: true,
        };
   }
-  start(id){
-    console.log(id);
+  start(){
+    this.setState({ isModalVisible: !this.state.isModalVisible });
+
+  }
+  apply(){
+    console.log("はい");
+    this.setState({ isModalVisible: !this.state.isModalVisible });
     Actions.main();
   }
   render() {
+    console.log(this.state.isModalVisible)
     return (
     <View style={entranceStyle.pageStyle}>
       <ImageBackground source={require('../../assets/carm_sea.jpg')} style={entranceStyle.image}>
         <View style={entranceStyle.upperButtonStyle}>
-          <CircleButton press={() => {this.start(1)}} text="三日間コース"/>
-          <CircleButton press={() => {this.start(2)}} text="一週間コース"/>
-          <CircleButton press={() => {this.start(3)}} text="一週間コース"/>
+          <CircleButton press={() => {this.start()}} text="三日間コース"/>
+          <CircleButton press={() => {this.start()}} text="一週間コース"/>
+          <CircleButton press={() => {this.start()}} text="一週間コース"/>
         </View>
+        <SelectModal visible={this.state.isModalVisible} press={() => {this.apply()}}/>
       </ImageBackground>
     </View>
     );
