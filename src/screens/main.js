3fada3a14e3adf08df086adea29040c3354f839b
kento <alpikinho0103kk@gmail.com>
2020-10-16 09:40:10 +0900

    mainページmodalやめ

--- src/screens/main.js
+++ src/screens/main.js
@@ -1,23 +1,19 @@
 import React, { Component } from 'react';
-import { View,Text,ImageBackground} from 'react-native';
-import GreeButton from '../../src/elements/atom/GreeButton';
+import { View,ImageBackground} from 'react-native';
 import OpningDatapicker from '../../src/components/OpningDatapicker';
-import Modal from "react-native-modal";
 import mainStyle from '../../src/style/mainStyle';
 
 class main extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      isModalVisible: true,
       date: new Date(),
-      isModalDisplay: false
+      isModalDisplay: true
        };
   }
   componentDidMount(){
   }
   start(){
-    this.setState({ isModalVisible: !this.state.isModalVisible });
     this.setState({ isModalDisplay: !this.state.isModalDisplay });
 
   }
@@ -31,12 +27,6 @@ class main extends Component {
     return (
     <View style={mainStyle.pageStyle}>
       <ImageBackground source={require('../../assets/carm_sea.jpg')} style={mainStyle.image}>
-        <Modal isVisible={this.state.isModalVisible}>
-          <View style={mainStyle.Modaltyle}>
-            <Text>Modal Content</Text>
-            <GreeButton press={() => {this.start()}} text="はい"/>
-          </View>
-        </Modal>
         <View style={mainStyle.DatePickerStyle}>
           {this.state.isModalDisplay ? <OpningDatapicker dated={this.state.date} change={(e,selectedDate) => {this.dateChange(e,selectedDate)}}/> : null}
         </View>
