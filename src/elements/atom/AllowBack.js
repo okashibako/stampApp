3d36f4a6bd77672795bd35bbb88cc491b1caa3a2
kento <alpikinho0103kk@gmail.com>
2021-03-04 23:19:44 +0900

    戻るボタンをコンポーネント

---
+++ src/elements/atom/AllowBack.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import { View, StyleSheet, TouchableWithoutFeedback, Image} from 'react-native';
+import { Actions } from 'react-native-router-flux';
+
+const AllowBack = () => {
+  
+    return (
+      <TouchableWithoutFeedback onPress={() => Actions.pop()}>
+        <Image source={require('../../../assets/fish.png')}style={styles.backArrowStyle} />
+      </TouchableWithoutFeedback>
+    );
+  };
+  const styles = StyleSheet.create({
+    backArrowStyle: {
+      resizeMode: 'contain',
+      width: 50,
+      height: 30,
+    },
+  });
+  export default AllowBack;
--- src/elements/molecules/MainNavigationBar.js
+++ src/elements/molecules/MainNavigationBar.js
@@ -1,16 +1,13 @@
 import React, { Component } from 'react';
-import { View, StatusBar,StyleSheet, TouchableWithoutFeedback, Image} from 'react-native';
-import { Actions, Router, Scene } from 'react-native-router-flux';
+import { View, StatusBar,StyleSheet } from 'react-native';
+import AllowBack from '../atom/AllowBack';
 
 class MainNavigationBar extends Component {
   render() {
     return (
-      <View >
-        <StatusBar />
-        <View style={styles.rowsComponent}>
-          <TouchableWithoutFeedback onPress={() => Actions.pop()}>
-            <Image source={require('../../../assets/fish.png')}style={styles.backarrowStyle} />
-          </TouchableWithoutFeedback>
+      <View style={styles.rowsComponent}>
+        <View style={styles.backarrowStyle}>
+          <AllowBack/>
         </View>
       </View>
     );
@@ -22,13 +19,9 @@ const styles = StyleSheet.create({
       backgroundColor: '#fff',  
     },
     backarrowStyle: {
-      resizeMode: 'contain',
-      flexDirection: 'row',
-      width: 40,
-      height: 60,
       left: 10,
-      top:0,
-      justifyContent: 'flex-start'
+      top:50,
+      height: 100,
     },
   });
   export default MainNavigationBar;
