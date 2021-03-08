048e3a85f23304c64cd64ed61bc2cc4a4ceb3972
asaba <swift0910@cpoint-lab.co.jp>
2021-03-08 17:29:00 +0900

    スタンプ履歴ページのルーター追加

--- App.js
+++ App.js
@@ -2,7 +2,7 @@ import React from 'react';
 import {Scene,Router} from 'react-native-router-flux';
 import entrance from './src/screens/entrance';
 import main from './src/screens/main';
-import MainNavigationBar from './src/elements/molecules/MainNavigationBar';
+import history from './src/screens/history';
 
 
 const App = () => (
@@ -10,6 +10,7 @@ const App = () => (
         <Scene key="root">
         <Scene key="entrance" initial component={entrance} title="entrance" hideNavBar/>
         <Scene key="main" component={main} title="main" hideNavBar/>
+        <Scene key="history" component={history} title="history" hideNavBar/>
         </Scene>
     </Router>
 );
