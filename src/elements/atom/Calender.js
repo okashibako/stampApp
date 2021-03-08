f65ebf6a2935767783a77a3a4a8842a2935c3334
asaba <swift0910@cpoint-lab.co.jp>
2021-03-08 17:26:51 +0900

    履歴ページに遷移する機能の追加

--- src/elements/atom/Calender.js
+++ src/elements/atom/Calender.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import {StyleSheet, TouchableWithoutFeedback, Image} from 'react-native';
+import {Actions} from 'react-native-router-flux';
 
 const Calender = () => {
   
     return (
-      <TouchableWithoutFeedback onPress={() => console.log("open!!")}>
+      <TouchableWithoutFeedback onPress={() => Actions.history()}>
         <Image source={require('../../../assets/calender.png')}style={styles.calenderLogoStyle} />
       </TouchableWithoutFeedback>
     );
