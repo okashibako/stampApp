fbcaa613c20b4c62de4c3131fd29e7e51d33df77
asaba <swift0910@cpoint-lab.co.jp>
2021-08-20 15:45:14 +0900

    モック関数追加

--- sum.js
+++ sum.js
@@ -1,4 +1,6 @@
-function sum(a, b) {
-  return a + b;
+function forEach(items, callback) {
+  for (let index = 0; index < items.length; index++) {
+    callback(items[index]);
+  }
 }
-export default sum;
+export default forEach;
