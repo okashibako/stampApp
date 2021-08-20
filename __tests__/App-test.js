aec797443af700611db2162c8e89e5d9ec6f7717
asaba <swift0910@cpoint-lab.co.jp>
2021-08-20 15:44:39 +0900

    テスト修正

--- __tests__/App-test.js
+++ __tests__/App-test.js
@@ -2,8 +2,15 @@
  * @format
  */
 
- import sum from '../sum';
+ import forEach from '../sum';
 
  test('adds 1 + 2 to equal 3', () => {
-   expect(sum(1, 2)).toBe(3);
- });
+  const mockCallback = jest.fn(x => 42 + x);
+  forEach([0, 1], mockCallback);
+  // The mock function is called twice
+  expect(mockCallback.mock.calls.length).toBe(2);
+  // The first argument of the second call to the function was 1
+  expect(mockCallback.mock.calls[1][0]).toBe(1);
+  // The return value of the first call to the function was 42
+  expect(mockCallback.mock.results[0].value).toBe(42);
+});
