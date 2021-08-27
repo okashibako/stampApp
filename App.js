efabff19023eae8deee96bd691761a65ad4a49a3
asaba <swift0910@cpoint-lab.co.jp>
2021-08-27 16:41:25 +0900

    react-navigation追加

--- App.js
+++ App.js
@@ -1,18 +1,45 @@
 import React from 'react';
 import {Scene,Router} from 'react-native-router-flux';
-import entrance from './src/screens/entrance';
-import main from './src/screens/main';
+import Entrance from './src/screens/entrance';
+import Main from './src/screens/main';
 import history from './src/screens/history';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { extendTheme, NativeBaseProvider, Button } from 'native-base';
 
-
-const App = () => (
-    <Router>
-        <Scene key="root">
-        <Scene key="entrance" initial component={entrance} title="entrance" hideNavBar/>
-        <Scene key="main" component={main} title="main" hideNavBar/>
-        <Scene key="history" component={history} title="history" hideNavBar/>
-        </Scene>
-    </Router>
-);
+const newColorTheme = {
+    brand: {
+        900: '#8287af',
+        800: '#7c83db',
+        700: '#b3bef6',
+    },
+};
+const theme = extendTheme({ colors: newColorTheme });
+function App(){
+    const Stack = createNativeStackNavigator();
+    return (
+        <NativeBaseProvider theme={theme}>
+        <NavigationContainer>
+          <Stack.Navigator>
+              <Stack.Screen name="entrance">
+                {props => <Entrance {...props} />}
+              </Stack.Screen>
+              <Stack.Screen name="main"
+                component={Main}
+                options={{
+                  headerRight: () => (
+                    <Button
+                      onPress={() => console.log("hello world")}
+                      title="Info"
+                      color="#fff"
+                    />
+                  ),
+                }}>
+              </Stack.Screen>
+          </Stack.Navigator>
+        </NavigationContainer>
+      </NativeBaseProvider>
+    );
+};
 
 export default App;
--- src/screens/entrance.js
+++ src/screens/entrance.js
@@ -1,54 +1,43 @@
-import React, { Component } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View,ImageBackground } from 'react-native';
 import CircleButton from '../../src/elements/atom/CircleButton';
-import {Actions} from 'react-native-router-flux';
 import entranceStyle from '../../src/style/entranceStyle';
 import SelectModal from '../elements/molecules/SelectModal';
-import AsyncStorage from '@react-native-community/async-storage';
-
-class entrance extends Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      isModalVisible: false,
-       };
+import Realm from "realm";
+const Cat = {
+  name: "Cat",
+  properties: {
+    _id: "objectId",
+    name: "string",
+    age: "int",
+    type: "string",
+  },
+};
+function entrance({ navigation }){
+  useEffect(() => {
+  });
+  function start(){
+    navigation.navigate('main');
   }
-  start(){
-    this.setState({ isModalVisible: !this.state.isModalVisible });
-
+  function apply(){
   }
-  async apply(){
-    this.setState({ isModalVisible: !this.state.isModalVisible });
-    try {
-      const value = await AsyncStorage.setItem('first_login', "1");
-      Actions.main();
-      if (value !== null) {
-        // We have data!!
-        Actions.main();
-        console.log(value);
-      }
-    } catch (error) {
-      // Error retrieving data
-    }
-
-  }
-  render() {
     return (
     <ImageBackground source={require('../../assets/carm_sea.jpg')} style={entranceStyle.image}>
       <View style={entranceStyle.pageStyle}>
           <View style={entranceStyle.upperButtonStyle}>
-            <CircleButton press={() => {this.start()}} text="三日間コース"/>
-            <CircleButton press={() => {this.start()}} text="一週間コース"/>
-            <CircleButton press={() => {this.start()}} text="一週間コース"/>
+            <CircleButton press={() => {start()}} text="三日間コース"/>
+            <CircleButton press={() => {start()}} text="一週間コース"/>
+            <CircleButton press={() => {start()}} text="一週間コース"/>
           </View>
+          {/*
           <SelectModal 
             visible={this.state.isModalVisible}
             press={() => {this.apply()}}
           />
+          */}
       </View>
     </ImageBackground>
     );
-  }
 }
 
 export default entrance;
