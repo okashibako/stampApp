28d75d6d23176c47a6bcd74eba81922bcc406d13
asaba <swift0910@cpoint-lab.co.jp>
2021-09-03 18:09:20 +0900

    realmオブジェクト追加

--- src/screens/entrance.js
+++ src/screens/entrance.js
@@ -2,14 +2,53 @@ import React, { useState, useEffect } from 'react';
 import { View,ImageBackground } from 'react-native';
 import CircleButton from '../../src/elements/atom/CircleButton';
 import entranceStyle from '../../src/style/entranceStyle';
-import SelectModal from '../elements/molecules/SelectModal';
+import Realm from "realm";
+
+const DogSchema = {
+  name: "Dog",
+  properties: {
+    name: "string",
+    age: "int?",
+  },
+};
+
 function entrance({ navigation }){
+    
   useEffect(() => {
-  });
-  function start(){
-    navigation.navigate('main');
+    read();
+  }, [])
+
+  async function read(){
+    let task;
+    try{
+      const realm = await Realm.open({
+        path: "myDog",
+        schema: [DogSchema],
+      });
+    task = realm.objects("Dog");
+    }catch(error){
+      console.log('エラー' + error.message);
+    }
+    if(task){
+      console.log("有りです")
+    } else {
+      console.log("無しです")
+    }
   }
-  function apply(){
+  async function start(){
+    let dog;
+    try{
+      const realm = await Realm.open({
+        path: "myDog",
+        schema: [DogSchema],
+      });
+      realm.write(() => {
+        dog = realm.create("Dog", { name: "Max", age: 5, });
+      });
+    }catch(error){
+      console.log('エラー' + error.message);
+    }
+    navigation.navigate('main');
   }
     return (
     <ImageBackground source={require('../../assets/carm_sea.jpg')} style={entranceStyle.image}>
@@ -19,12 +58,6 @@ function entrance({ navigation }){
             <CircleButton press={() => {start()}} text="一週間コース"/>
             <CircleButton press={() => {start()}} text="一週間コース"/>
           </View>
-          {/*
-          <SelectModal 
-            visible={this.state.isModalVisible}
-            press={() => {this.apply()}}
-          />
-          */}
       </View>
     </ImageBackground>
     );
